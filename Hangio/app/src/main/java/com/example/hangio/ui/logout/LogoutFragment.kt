310cfeeb6e5c0385d9484f2a6b392f75852a9078
Luka Gotovac <37194110+gotovac@users.noreply.github.com>
2020-02-07 02:36:00 +0100

    Kreiran pop-up dialog prilikom odabira opcije Odjavi u glavnom meniju

--- Hangio/app/src/main/java/com/example/hangio/ui/logout/LogoutFragment.kt
+++ Hangio/app/src/main/java/com/example/hangio/ui/logout/LogoutFragment.kt
@@ -1,5 +1,6 @@
 package com.example.hangio.ui.logout
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -19,14 +20,28 @@ class LogoutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        FirebaseAuth.getInstance().signOut()
+        val builder = AlertDialog.Builder(this.activity)
+        builder.setTitle("Odjava")
+        builder.setMessage("Jeste li sigurni da se želite odjaviti?")
+        builder.setPositiveButton("Da"){
+                dialog, which ->
+            FirebaseAuth.getInstance().signOut()
 
-        val intent =
-            activity!!.baseContext.packageManager.getLaunchIntentForPackage(activity!!.baseContext.packageName)
-        intent!!.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        startActivity(intent)
-        android.os.Process.killProcess(android.os.Process.myPid())
-        exitProcess(0)
+            val intent =
+                activity!!.baseContext.packageManager.getLaunchIntentForPackage(activity!!.baseContext.packageName)
+            intent!!.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            android.os.Process.killProcess(android.os.Process.myPid())
+            exitProcess(0)
+        }
+        builder.setNegativeButton("Ne"){
+                dialog, which ->
+            this.activity!!.onBackPressed()
+        }
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
 
         return null
     }
